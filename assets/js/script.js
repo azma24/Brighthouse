42b216031a7c5203fa41e33fe6892aeca7e923c0
azma24 <278310366+azma24@users.noreply.github.com>
2026-07-17 11:44:57 +0900

    feat: ハンバーガーメニューを追加しヘッダーをレスポンシブ対応
    - ハンバーガーメニューを追加
    - ヘッダーのメディアクエリを追加
    - ハンバーガーメニュー対応に伴いヘッダーナビゲーションのHTML構造を修正
    - flex中央揃え用のmixinを追加

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ const toTopButton = document.querySelector(".to-top");
 
 // スクロールしたら表示
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 300) {
+  if (window.scrollY > 700) {
     toTopButton.classList.add("is-show");
   } else {
     toTopButton.classList.remove("is-show");
@@ -16,3 +16,25 @@ toTopButton.addEventListener("click", () => {
     behavior: "smooth",
   });
 });
+
+// ハンバーガーメニュー
+const hamburger = document.querySelector(".hamburger");
+const spNav = document.querySelector(".header__menu");
+const overlay = document.querySelector(".overlay");
+
+function toggleMenu() {
+  hamburger.classList.toggle("active");
+  spNav.classList.toggle("open");
+  overlay.classList.toggle("open");
+
+  const isOpen = hamburger.classList.contains("active");
+  hamburger.setAttribute("aria-expanded", isOpen);
+}
+
+hamburger.addEventListener("click", toggleMenu);
+
+document.querySelectorAll(".header_menu a").forEach((link) => {
+  link.addEventListener("click", toggleMenu);
+});
+
+overlay.addEventListener("click", toggleMenu);
